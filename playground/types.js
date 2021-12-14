48cdd9e03bdc82ce2ae31f12d9cb61502c090472
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-12-14 17:00:14 +0000

    Modulo operator

--- playground/types.js
+++ playground/types.js
@@ -1,5 +1,25 @@
+/* eslint-disable */
 const age = 100;
 const name = 'Mark';
 
 console.log(typeof age);
 console.log(typeof name);
+
+// If you do maths with non numbers you get concatenation. 'The plus sign is loaded!!'
+const uhOh = 1 + "1"
+console.log('UH OH -', typeof(uhOh));
+
+console.log(Math.round(20.5)); // Rounds up or down
+console.log(Math.round(20.3)); // Rounds up or down
+console.log(Math.floor(20.5)); // Rounds down to nearest whole number
+console.log(Math.ceil(20.2)); // Rounds up to nearest whole number
+console.log(Math.random()); // random
+
+// Dividing 20 sweets between 3 kids
+const sweets = 20;
+const kids = 3;
+const eachKidGets = Math.floor(sweets / kids); // Rounds down to nearest whole number
+console.log(`Each kid gets ${eachKidGets} sweets`);
+// What about the ones left over?
+const dadGets = sweets % kids; // Modulo operator gives number left over after division
+console.log(`Dad gets ${dadGets} sweets`);

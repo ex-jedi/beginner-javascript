9f62b32e1d8d2691d5baa3ec2d217f70a4889de0
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-12-14 16:31:06 +0000

    Multiline strings

--- playground/types.js
+++ playground/types.js
@@ -3,3 +3,20 @@
 const name = 'Mark';
 const middle = "Robert";
 const last = `Phoenix`;
+
+// Escaping character (Better to use double quotes or backtics here)
+const sentence = 'He\'s too cool'
+
+// Using backtics
+const sentence2 = `He's too cool`
+
+// Multiple line String
+
+const song = `Get
+back
+to
+where
+you
+once
+belonged!
+`

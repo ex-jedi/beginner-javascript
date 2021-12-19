2a6865c5eb2a9b48a23fdc41db378e5d867bce52
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-12-19 16:26:32 +0000

    More params

--- playground/custom-functions/cf.js
+++ playground/custom-functions/cf.js
@@ -1,14 +1,22 @@
 // Function definition
 function calculateBill(billAmount, taxRate) {
   // This is the function body
-  console.log('Running Calculate Bill!');
+  // console.log('Running Calculate Bill!');
 
   const total = billAmount * (1 + taxRate);
 
   return total;
 }
 
+const myBill = 384;
+const myTax = 0.26;
+
 //  Function call or *run*
-const myTotal = calculateBill(100, 0.13);
-const myTotal2 = calculateBill(500, 0.13);
-console.log(myTotal, myTotal2);
+const myTotal = calculateBill(myBill, myTax);
+
+function sayHiTo(firstName) {
+  return `Hello ${firstName}`;
+}
+
+const greeting = sayHiTo('Karl');
+console.log(greeting);

2fa960f5475968a6b3fcc6b90cf36d3c1e697437
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-12-21 08:31:43 +0000

    Function expression and anonymous.

--- playground/custom-functions/ways-to-make-a-function.js
+++ playground/custom-functions/ways-to-make-a-function.js
@@ -1,1 +1,14 @@
-console.log('Hi');
+// function doctorize(firstName) {
+//   return `Dr ${firstName}`;
+// }
+
+// Anonymous function
+// function (firstName) {
+//   return `Dr ${firstName}`;
+// }
+
+// Function expression
+// Stores function value as a variable. Technically an anonymous function.
+const doctorise = function (firstName) {
+  return `Dr ${firstName}`;
+};

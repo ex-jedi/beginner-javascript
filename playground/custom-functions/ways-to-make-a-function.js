9cb6e720b003aebf2780269d56962a3e975f481a
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-12-21 08:34:22 +0000

    Hoisting

--- playground/custom-functions/ways-to-make-a-function.js
+++ playground/custom-functions/ways-to-make-a-function.js
@@ -7,8 +7,18 @@
 //   return `Dr ${firstName}`;
 // }
 
+// Wont work before it's declared
+doctorize('Mark');
+
+// Does work before it's declared and it's 'hoisted'
+doctorize2('Mark');
+
 // Function expression
 // Stores function value as a variable. Technically an anonymous function.
 const doctorise = function (firstName) {
   return `Dr ${firstName}`;
 };
+
+function doctorize2(firstName) {
+  return `Dr ${firstName}`;
+}
